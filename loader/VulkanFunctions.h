a33f106570bea77797b2f910f6c4585d74a6f5c7
Venku628 <huebnerfalk95@gmail.com>
2019-07-31 17:58:14 +0200

    Initial commit.

---
+++ loader/VulkanFunctions.h
@@ -0,0 +1,14 @@
+#pragma once
+#define VK_NO_PROTOTYPES
+#include "vulkan/vulkan.h"
+
+namespace MelonRenderer {
+#define EXPORTED_VULKAN_FUNCTION( name ) extern PFN_##name name;
+#define GLOBAL_LEVEL_VULKAN_FUNCTION( name ) extern PFN_##name name;
+#define INSTANCE_LEVEL_VULKAN_FUNCTION( name ) extern PFN_##name name;
+#define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension ) extern PFN_##name name;
+#define DEVICE_LEVEL_VULKAN_FUNCTION( name ) extern PFN_##name name;
+#define DEVICE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension ) extern PFN_##name name;
+
+#include "ListOfVulkanFunctions.inl"
+}
