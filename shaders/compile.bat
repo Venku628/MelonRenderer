51558c6cb7b8fc4a5bbaf2c01a431576a6a1e715
Venku628 <huebnerfalk95@gmail.com>
2019-12-04 08:48:29 +0100

    Fixed hard coded vulkan sdk path in shader compile bat.

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,2 +1,2 @@
-E:/VulkanSDK/1.1.121.2/Bin32/glslc.exe shaders/shader.vert -o shaders/vert.spv
-E:/VulkanSDK/1.1.121.2/Bin32/glslc.exe shaders/shader.frag -o shaders/frag.spv
+%VULKAN_SDK%/Bin32/glslc.exe shaders/shader.vert -o shaders/vert.spv
+%VULKAN_SDK%/Bin32/glslc.exe shaders/shader.frag -o shaders/frag.spv
